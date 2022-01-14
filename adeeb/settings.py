8ee9a980db0c979c74c1b593e7d7c4d1d17986f9
adzim99 <adzimnikman99@gmail.com>
2022-01-14 16:32:45 +0800

    v1.3

--- adeeb/settings.py
+++ adeeb/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-639#+zp=3tqe#uc5-fht!)tmg4e9843kk12m_1ga77h$yf)lg_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['labhirarc.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
